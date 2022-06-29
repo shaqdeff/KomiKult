a6f4098d7d77734530c9a07174fe3bc358281060
shaqdeff <shaquillendunda@gmail.com>
2022-06-29 10:12:19 +0300

    Add header

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,10 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(jpg|jpeg|png)$/,
+        type: 'asset/resource',
+      }
     ],
   },
   devServer: {
